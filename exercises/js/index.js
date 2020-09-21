c5d017254dfbd9c31dd4c66047c9d1f18614310e
Ivan Davis <techbyivan@users.noreply.github.com>
2020-09-20 21:36:36 -0400

    updated navigation
    updated navigation

--- exercises/js/index.js
+++ exercises/js/index.js
@@ -21,18 +21,21 @@ const buildPosts = (blogPosts) => {
 	let blogPostsContent = "";
 	for(blogPost of blogPosts) {
 		const postDate = new Date(parseInt(blogPost.added_date)).toDateString();
-		const postImage = `${API_BASE_URL}${blogPost.post_image}`;
-		blogPostsContent += ` 
-        <div class="post">
-					<div class="post-image" style="background-image: url(${postImage})"></div>
-					<div class="post-content">
-						<div class="post-date">${postDate}</div>
-						<div class="post-title"><h4>${blogPost.title}</h4></div>
-						<div class="post-text">
-							${blogPost.content}
-						</div>
-					</div>
-                </div>
+        const postImage = `${API_BASE_URL}${blogPost.post_image}`;
+        const postLink = `/post.html?id=${blogPost.id}`
+        blogPostsContent += ` 
+        <a class="post-link" href="${postLink}">
+            <div class="post">
+                        <div class="post-image" style="background-image: url(${postImage})"></div>
+                        <div class="post-content">
+                            <div class="post-date">${postDate}</div>
+                            <div class="post-title"><h4>${blogPost.title}</h4></div>
+                            <div class="post-text">
+                                ${blogPost.content}
+                            </div>
+                        </div>
+                    </div>
+                </a>
         `
 	}
     document.querySelector(".blog-posts").innerHTML= blogPostsContent; 
